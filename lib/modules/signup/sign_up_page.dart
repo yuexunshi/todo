1d016946932b379b247f0383b813aee21cdd5b4b
shiyuexun <yuexunshi@91jsys.com>
2020-12-11 00:28:53 +0800

    ✨feat: 加入数据库

--- lib/modules/signup/sign_up_page.dart
+++ lib/modules/signup/sign_up_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:todo/modules/singinup/sign_up_controller.dart';
-
+import 'package:todo/modules/signup/sign_up_controller.dart';
 import 'components/body.dart';
 
 class SignUpPage extends GetView<SignUpController> {
